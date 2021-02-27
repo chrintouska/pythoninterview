84e0ad60894e8bd83b3fe722b773bfcbc9106144
chrintouska <79751503+chrintouska@users.noreply.github.com>
2021-02-27 14:50:13 +0200

    Add files via upload

---
+++ exersize.py
@@ -0,0 +1,31 @@
+import csv
+filename="adjacencies(1).csv" 
+with open(filename,'r') as data: 
+   for line in csv.reader(data): 
+        print(line) 
+class Adjacency(object):
+    def __init__(self,object_id,priority,distance,tier,attemps,hosr):
+        self.object_Id=object_Id
+        self.priority=priority
+        self.distance=distance
+        self.tier=tier
+        self.attempts=attempts
+        self.hosr=hosr
+for line in csv.reader(data):
+    if (self.priority==true):
+        sorted(iterable,\,*,key=None,reverse=False)
+        print(line)
+for line in csv.reader(data):
+    if (self.priority==true)
+        if (self.priority==1):
+            sorted(self.distance,key=None,reverse=True)
+            print(line)
+        if (self.priority==2):
+            sorted(self.tier,key=None,reverse=True)
+            print(line)
+        if (self.priority==3):
+            sorted(self.atempts,key=None,reverse=False)
+            print(line)
+        if (self.priority==4):
+            sorted(self.hosr,key=None,reverse=False)
+            print(line)
